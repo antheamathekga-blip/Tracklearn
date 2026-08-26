901e984178a7c93d7cc4a9fbf85554058fc825ce
antheamathekga-blip <antheamathekga@gmail.com>
2026-08-26 21:51:44 +0200

    Refactor Student class and update constructors
    Refactored Student class to remove unused fields and constructors, and added new fields for course, progress, and score.

--- Student.java
+++ Student.java
@@ -1,49 +1,14 @@
 public class Student {
-    private String id;
-    private String name;
-    private String activity;
-    private double hours;
-    private int attendance;
-    private double averageMark;
-    private String riskStatus;
+    public String name;
+    public String course;
+    public int progress;
+    public int score;
 
-    // Constructor for Main.java (Banele's task - TrackLearn)
-    public Student(String name, String activity, double hours) {
-        this.name = name;
-        this.activity = activity;
-        this.hours = hours;
-        this.id = "N/A";
-        this.attendance = 0;
-        this.averageMark = hours; // use hours as mark for now
-        this.riskStatus = "Low";
+    public Student(String name, String course, int progress, int score) {
+        this.name = name; this.course = course; this.progress = progress; this.score = score;
     }
 
-    // Constructor for Siviwe/Phumelele's model
-    public Student(String id, String name, int attendance, double averageMark) {
-        this.id = id;
-        this.name = name;
-        this.attendance = attendance;
-        this.averageMark = averageMark;
-        this.activity = "General";
-        this.hours = averageMark;
-        this.riskStatus = "Low";
-    }
-
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public String getActivity() { return activity; }
-    public double getHours() { return hours; }
-    public int getAttendance() { return attendance; }
-    public double getAverageMark() { return averageMark; }
-    public String getRiskStatus() { return riskStatus; }
-    
-    public void setRiskStatus(String riskStatus) {
-        this.riskStatus = riskStatus;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{name='" + name + "', activity='" + activity + "', hours=" + hours + 
-               ", attendance=" + attendance + ", averageMark=" + averageMark + "}";
+    public boolean isBehind() {
+        return progress < 50;
     }
 }
