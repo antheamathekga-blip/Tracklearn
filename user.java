1b91435afa0270da005e27c4e193a9c27734124f
antheamathekga-blip <antheamathekga@gmail.com>
2026-08-26 21:50:36 +0200

    Create User class with properties and constructor

---
+++ user.java
@@ -0,0 +1,11 @@
+public class User {
+    public int id;
+    public String name;
+    public String email;
+    public String role;
+    public String initials;
+
+    public User(int id, String name, String email, String role, String initials) {
+        this.id = id; this.name = name; this.email = email; this.role = role; this.initials = initials;
+    }
+}
